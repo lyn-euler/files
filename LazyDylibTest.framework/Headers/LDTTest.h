48f8d2f38aaa942c3af69af10bea4916608c753f
ly0u <asynclog@163.com>
2022-03-10 17:09:23 +0800

    sign framework

---
+++ LazyDylibTest.framework/Headers/LDTTest.h
@@ -0,0 +1,21 @@
+//
+//  LDTTest.h
+//  LazyDylibTest
+//
+//  Created by ello on 2022/3/1.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LDTTest : NSObject
+
+@property (nonatomic, strong, readonly) NSString *text;
+
+-(void)log;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
